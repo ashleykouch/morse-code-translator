a5c84a0cbc96d7c48f47fcb2896af50ffbaa91e8
ashleykouch <ashleykouch99@gmail.com>
2022-11-30 17:05:17 +1100

    created function for translation from latin to morse code

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// import characterMapping from "./characters.js";
+// import { characterMapping } from "./characters.js";
 
 // function textToMorseCode() {
 //   const textInput = document.getElementById("textInput").value;
@@ -16,3 +16,84 @@
 
 //   document.getElementById("morseCodeInput").value = code;
 // }
+
+// create mapping of characters from latin to morse code
+const latinToMorseCode = {
+  // latin to morse code
+  A: ".-",
+  B: "-...",
+  C: "-.-.",
+  D: "-..",
+  E: ".",
+  F: "..-.",
+  G: "--.",
+  H: "....",
+  I: "..",
+  J: ".---",
+  K: "-.-",
+  L: ".-..",
+  M: "--",
+  N: "-.",
+  O: "---",
+  P: ".--.",
+  Q: "--.-",
+  R: ".-.",
+  S: "...",
+  T: "-",
+  U: "..-",
+  W: ".--",
+  X: "-..-",
+  Y: "-.--",
+  Z: "--..",
+
+  //   numbers to morse code
+  0: "-----",
+  1: "..---",
+  2: "..---",
+  3: "...--",
+  4: "....-",
+  5: ".....",
+  6: "-....",
+  7: "--...",
+  8: "---..",
+  9: "----.",
+
+  //   puncutation to morse code
+
+  ".": ".-.-.-",
+  ",": "--..--",
+  "?": "..--..",
+  ":": "---...",
+  "/": "-..-.",
+  "-": "-....-",
+  "=": "-...-",
+  "(": "-.--.",
+  ")": "-.--.-",
+  "&": ".-...",
+  " ": "/",
+};
+
+//   - identify/check if the string is in english
+//   - might throw error if it is not in english
+// - split string
+
+// - have a function that takes in a string in english
+const convertToMorseCode = (string) => {
+  return (
+    string
+      // convert string to uppercase
+      .toUpperCase()
+      // we want an array of individual characters (split with an empty string)
+      .split("")
+      // use .map iterator to grab array of english characters and match to morse code characters
+      .map((element) => {
+        return latinToMorseCode[element]
+          ? latinToMorseCode[element]
+          : [element];
+      })
+      // convert array back to string using .join with a space (" ")
+      .join(" ")
+  );
+};
+
+console.log(convertToMorseCode("SOS SOS"));
